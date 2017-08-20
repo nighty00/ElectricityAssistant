8891b8076baf75d0f0dfe790b9a211361a05815c
Zhou Zhou <nighty1014@gmail.com>
2017-08-20 13:44:17 +1000

    20/08

--- ElectricityAssistant/src/app/shared/guard/auth.guard.ts
+++ ElectricityAssistant/src/app/shared/guard/auth.guard.ts
@@ -8,11 +8,12 @@ export class AuthGuard implements CanActivate {
     constructor(private router: Router) { }
 
     canActivate() {
-        if (localStorage.getItem('isLoggedin')) {
-            return true;
-        }
+        // if (localStorage.getItem('isLoggedin')) {
+        //     return true;
+        // }
 
-        this.router.navigate(['/login']);
-        return false;
+        // this.router.navigate(['/login']);
+        // return false;
+        return true;
     }
 }
